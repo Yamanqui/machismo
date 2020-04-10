057625296be2a8db3ff58597fce45eee61abada1
Yamanqui García Rosales <yamanqui@gmail.com>
2020-04-10 12:01:23 -0600

    Updates

---
+++ precache-manifest.768b05d489bcbe2dd351cfa1f2c0c5bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7b25bc93287e6fa7593ebf34b9c8a7",
+    "url": "/machismo/index.html"
+  },
+  {
+    "revision": "04ed880698524dec8eac",
+    "url": "/machismo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "84c8d1face85b8598027",
+    "url": "/machismo/static/js/2.445f9959.chunk.js"
+  },
+  {
+    "revision": "f74a6debb7da89320918198b818026f8",
+    "url": "/machismo/static/js/2.445f9959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ed880698524dec8eac",
+    "url": "/machismo/static/js/main.b49319a8.chunk.js"
+  },
+  {
+    "revision": "411205369762f1f65e15",
+    "url": "/machismo/static/js/runtime-main.e8887620.js"
+  },
+  {
+    "revision": "22f42be4e245bf2b00a65bf9636b1c36",
+    "url": "/machismo/static/media/Lentes.22f42be4.svg"
+  }
+]);
